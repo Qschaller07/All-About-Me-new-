abf0bb0e144d767e26ed58e56695fa907813de0c
Jonas <jonas.gregory@outlook.com>
2024-08-21 09:06:20 -0500

    views

---
+++ All About Me (new)/JonasView.swift
@@ -0,0 +1,18 @@
+//
+//  JonasView.swift
+//  All About Me (new)
+//
+//  Created by Jonas R. Gregory on 8/21/24.
+//
+
+import SwiftUI
+
+struct JonasView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JonasView()
+}
