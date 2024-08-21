18e2274673309ff315d16f667716eb8f99477d6b
Jonas <jonas.gregory@outlook.com>
2024-08-21 09:15:50 -0500

    jcfvghjkl

--- All About Me (new)/ContentView.swift
+++ All About Me (new)/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationStack {
+                NavigationLink {
+                    JonasView()
+                } label: {
+                    Text("Jonas")
+                }
+
+            }
         }
         .padding()
     }
