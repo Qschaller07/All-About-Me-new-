0a45d71c67b8f5964549d74bd7db406a93fcb3e5
Quin Schaller <qaschaller@gmail.com>
2024-08-21 09:00:43 -0500

    Initial Commit

---
+++ All About Me (new)/All_About_Me__new_App.swift
@@ -0,0 +1,17 @@
+//
+//  All_About_Me__new_App.swift
+//  All About Me (new)
+//
+//  Created by Quin Schaller on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct All_About_Me__new_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ All About Me (new)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  All About Me (new)
+//
+//  Created by Quin Schaller on 8/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
